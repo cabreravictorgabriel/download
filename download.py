6ba7463d6f7500569a9c87fb036015a81bd5dbf2
cabreravictorgabriel <cabreravictorgabriel@gmail.com>
2021-04-29 23:25:07 -0300

    baixar mais de um video ao mesmo tempo, adicionando nome ao arquivo

--- download.py
+++ download.py
@@ -1,21 +1,28 @@
 import requests
-request = requests.get('http://fast.wistia.net/embed/iframe/ldvcdu9sj6')
-texto = request.text
-final = texto.find("bin")
-inicio = texto.find("http://embed")
-url = (texto[inicio:final]) + 'mp4'
-r = requests.get(url)
-f = open('video1.mp4', 'wb')
-for chunk in r.iter_content(chunk_size=255):
-    if chunk:
-        f.write(chunk)
-f.close()
+wvideo = '3z0lxlu9p3', 'ldvcdu9sj6'
+for c in range(0, len(wvideo)):
+    aula = 'aula1.mp4', 'aula2.mp4'
+    request = requests.get(f'http://fast.wistia.net/embed/iframe/{wvideo[c]}')
+    texto = request.text
+    final = texto.find("bin")
+    inicio = texto.find("http://embed")
+    url = (texto[inicio:final]) + 'mp4'
+    r = requests.get(url)
+    f = open(aula[c], 'wb')
+    for chunk in r.iter_content(chunk_size=255):
+        if chunk:
+            f.write(chunk)
+    f.close()
 
 
 '''
 wvideo = '3z0lxlu9p3'
 for c in wvideo:
+
     request = requests.get(f'http://fast.wistia.net/embed/iframe/{wvideo}')
-    print(request.content)'''
+
+    print(request.content)
+'''
+
 
 # http://fast.wistia.net/embed/iframe/3z0lxlu9p3
